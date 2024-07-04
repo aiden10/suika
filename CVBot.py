c8c426e3fecb31fdeaff8717ea10ec4e693f2c9c
aiden10 <aiden.ten30@gmail.com>
2024-07-04 04:31:13 -0400

    basic functionality

--- CVBot.py
+++ CVBot.py
@@ -1,61 +1,28 @@
-"""
-using opencv
-use HoughCircles function to get list of detected circles
-get the average rgb of each circle, do that a few times to determine the average rgb of each fruit, and sum them to get a unique value for each fruit
-also store the average radius of each fruit
-compare each cirlces summed average rgb (look at nearest value) to the known fruit values to determine what each fruit is 
-
-go over each fruit's position (hopefully x,y is middle of circle) and see if it overlaps with any other fruit
-assign each fruit on the board a unique key
-Example:
-detection_threshold = 5 # pixels
-touching_fruits = {}
-for fruit in fruits:
-    current_fruit_x, current_fruit_y, current_fruit_radius, current_fruit_key = fruit[0], fruit[1], fruit[2], fruit[3]
-    for other_fruit in fruits:
-        other_fruit_x, other_fruit_y, other_fruit_radius, other_fruit_key = other_fruit[0], other_fruit[1], other_fruit[2], other_fruit[3]
-        
-        touching = (abs(current_fruit_radius - other_fruit_radius) <= 
-        (math.sqrt((current_fruit_x - other_fruit_x)^2 + (current_fruit_y - other_fruit_y)^2)) <= 
-        ((current_fruit_radius + other_fruit_radius + detection_threshold)))
-
-        if touching:
-            exists = graph.find(current_fruit_key) # imaginary graph class and function which goes over each node and finds it
-            if exists:
-                touching_fruits.add(fruit) # use a set so there are no duplicates
-                touching_fruits.add(other_fruit)
-                graph.add(fruit, other_fruit) # finds the first argument and connects it to the second argument
-
-I'm guessing that a graph could be implemented the same way that binary trees are except instead of a single pointer to
-its next node, it'd have a list of pointers to its next nodes
-the graph also needs to be directed to be able to find the longest path but if they're touching then they would be pointing to eachother
-search the graph to find the best fruit chain. 
-ideal fruit chain would be increasing rank by one each node
-so each node/fruit should also have its rank 
-cherry -> strawberry -> grape -> dekopon, etc
-Traversing:
-paths = {}
-for fruit in touching_fruits:
-    length = graph.find_longest_path(fruit) # this function would need to prioritize the connections where rank increases by one 
-    path = {fruit: length}
-
-longest_chain = max(paths.items()) 
-fruit, length = longest_chain.items()[0]
-
-fruit_x, fruit_y = fruit[0], fruit[1]
-pyautogui.click(fruit_x, fruit_y)
-although I would also need to check if the space above the fruit is clear or not before dropping it... 
-
-after the graph has been created and the fruits are there it'd be cool to see a visualization        
-"""
 import numpy as np
 import cv2
+import math
+import pyautogui
+import random
+import time
 from mss import mss
 from bisect import bisect_left
 from PIL import Image
 
+class Fruit:
+    def __init__(self, x, y, radius, value):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.connections = []
+        self.value = value
+    def __str__(self):
+        return list(FRUIT_VALUES_TABLE.keys())[list(FRUIT_VALUES_TABLE.values()).index(self.value)]
+    def __repr__(self):
+        return self.__str__()
+    
 # might modify this to have a list for r, g, and b values for each fruit
 COLOR_KEYS = [500, 540, 565, 580, 590, 610, 625, 640, 650, 675, 700, 710] 
+EXTRA_RADIUS = 15
 FRUIT_TABLE = {
     500: 'unknown',
     540: 'cherry',
@@ -71,6 +38,22 @@ FRUIT_TABLE = {
     640: 'unknown',
     650: 'melon'
 }
+FRUIT_VALUES_TABLE = {
+    'cherry': 1,
+    'strawberry': 2,
+    'grape': 3,
+    'dekopon': 4,
+    'orange': 5,
+    'apple': 6,
+    'pear': 7,
+    'peach': 8,
+    'pineapple': 9,
+    'melon': 10,
+    'watermelon': 11
+}
+bounding_box = {'top': 175, 'left': 715, 'width': 490, 'height': 800}
+sct = mss()
+
 def take_closest(myList, myNumber):
     pos = bisect_left(myList, myNumber)
     if pos == 0:
@@ -83,7 +66,96 @@ def take_closest(myList, myNumber):
         return after
     else:
         return before
+
+def is_intersecting(fruit1, fruit2):
+    x1, y1, r1 = fruit1.x, fruit1.y, fruit1.radius + EXTRA_RADIUS
+    x2, y2, r2 = fruit2.x, fruit2.y, fruit2.radius + EXTRA_RADIUS
+    d = math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+    return (d <= r2 - r1) or (d <= r1 - r2) or (d < r1 + r2) or (d == r1 + r2)
+
+def get_intersections(fruits):
+    # returns list of fruit objects
+    adjacent_fruits = set()
+    for f in fruits:
+        # create first fruit object
+        circle_data1 = f[0]
+        name = f[1]
+        value = FRUIT_VALUES_TABLE[name]
+        fruit = Fruit(circle_data1[2][0], circle_data1[2][1], circle_data1[1], value)
+        for of in fruits:
+            # create second fruit object
+            circle_data2 = of[0]
+            name = of[1]
+            value = FRUIT_VALUES_TABLE[name]
+            other_fruit = Fruit(circle_data2[2][0], circle_data2[2][1], circle_data2[1], value)
+
+            if fruit.x != other_fruit.x and fruit.y != other_fruit.y: # don't want to reference the same fruit
+                if is_intersecting(fruit, other_fruit):
+                    fruit.connections.append(other_fruit)
+                    adjacent_fruits.add(fruit)
     
+    return list(adjacent_fruits)
+
+def get_chain_recursive(fruit, first_fruit, current_fruit, visited=None):
+    if visited is None:
+        visited = set()
+    
+    if fruit in visited:
+        return 0
+    
+    visited.add(fruit)
+    max_chain = 0
+
+    for adjacent_fruit in fruit.connections:
+        if adjacent_fruit not in visited:
+            value_difference = abs(adjacent_fruit.value - current_fruit.value)
+            if value_difference <= 3:
+                chain_score = get_chain_recursive(adjacent_fruit, first_fruit, current_fruit, visited)
+                if value_difference == 1:
+                    chain_score += 4
+                if value_difference == 0:
+                    chain_score += 3
+                elif value_difference == 2:
+                    chain_score += 2
+                elif value_difference == 3:
+                    chain_score += 1
+                
+                max_chain = max(max_chain, chain_score)
+    
+    visited.remove(fruit)
+    return max_chain
+
+def get_chain(fruit, current_fruit):
+    return get_chain_recursive(fruit, fruit, current_fruit)
+
+def is_fruit_covered(fruit, fruits):
+    x_threshold = 50
+    for other_fruit in fruits:
+        if abs(fruit.x - other_fruit.x) < x_threshold and other_fruit.y > fruit.y:
+            return True
+        
+    return False
+
+def place_fruit(fruits, current_fruit):
+    current_fruit_object = Fruit(0, 0, 0, FRUIT_VALUES_TABLE[current_fruit])
+    chains = {}
+    for fruit in fruits:
+        chain = get_chain(fruit, current_fruit_object)
+        chains.update({fruit: chain})
+
+    chains = dict(sorted(chains.items(), key=lambda item: item[1], reverse=True)) 
+    for chain in chains:
+        fruit = chain
+        if not is_fruit_covered(fruit, fruits):
+            pyautogui.click(fruit.x + 715, fruit.y + 175)
+            print(fruit.connections)
+            return
+        else:
+            pyautogui.click(715 + random.randint(0, 300), 500)
+            return
+    
+    pyautogui.click(715 + random.randint(0, 300), 500) # for if there are no detected fruits 
+
 def identify_fruit(circle_data):
     color_sum = circle_data[0]
     radius = circle_data[1]
@@ -112,14 +184,13 @@ def identify_fruit(circle_data):
         return 'melon'
     else:
         return 'unknown'
-    
-def identify_circles(frame):
-    
+
+def identify_circles(frame, wait):
     fruits = []
     frame = np.asarray(frame)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     circles = cv2.HoughCircles(gray_frame, cv2.HOUGH_GRADIENT, 1.2, 40, param1=110,param2=50,minRadius=10,maxRadius=180)
-    ys = []
+    ys = {}
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for circle in circles[0, :]:
@@ -128,30 +199,33 @@ def identify_circles(frame):
             avg_rgb = cv2.mean(frame, mask=mask)[::-1]
             circle_data = (int((avg_rgb[0] + avg_rgb[1] + avg_rgb[2])), circle[2], (circle[0], circle[1]))
             fruit = identify_fruit(circle_data) 
-            # fruits.append((round(avg_rgb[0], 2), round(avg_rgb[1], 2), round(avg_rgb[2], 2)))
             if fruit != 'unknown':
                 fruits.append((circle_data, fruit))
-                ys.append({fruit : circle[1]})
+                ys.update({fruit : circle[1]})
                 cv2.putText(frame, fruit, (circle[0], circle[1]), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
                 cv2.circle(frame, (circle[0], circle[1]), circle[2], (0, 255, 0), 2) # draw the outer circle
                 cv2.circle(frame, (circle[0], circle[1]), 2, (0, 0, 255), 3) # draw the center of the circle
+
         fruits = sorted(fruits, key=lambda x: x[1])
-        print(fruits)
-        ys_sorted = sorted(ys, key=lambda d: list(d.values())[0])
-        if len(ys_sorted) > 0:
-            highest_fruit = next(iter(ys_sorted[0].keys()))
-            cv2.putText(frame, f'Dropping: {highest_fruit}', (185, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        connections = get_intersections(fruits)
+        ys_sorted = dict(sorted(ys.items(), key=lambda item: item[1]))
+        highest_fruit = next(iter(ys_sorted))
+        cv2.putText(frame, f'Dropping: {highest_fruit}', (185, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        if wait == 0:
+            place_fruit(connections, highest_fruit)
     return frame
 
-bounding_box = {'top': 175, 'left': 715, 'width': 490, 'height': 800}
+def main():
+    wait = 0
+    while True:
+        if wait == 30: wait = 0
+        sct_img = sct.grab(bounding_box)
+        frame = identify_circles(sct_img, wait)
+        cv2.imshow('screen', np.array(frame))
+        cv2.setWindowProperty('screen', cv2.WND_PROP_TOPMOST, 1)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        wait += 1
 
-sct = mss()
-
-while True:
-    sct_img = sct.grab(bounding_box)
-    frame = identify_circles(sct_img)
-    cv2.imshow('screen', np.array(frame))
-    cv2.setWindowProperty('screen', cv2.WND_PROP_TOPMOST, 1)
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+main()
